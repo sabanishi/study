988427226ed6ba74768d6a56ffab95293cde4242
sabanishi <84651801+sabanishi@users.noreply.github.com>
2024-08-21 01:12:28 +0900

    Update vectorize_emb.py

--- GitHubActions/vectorize_emb.py
+++ GitHubActions/vectorize_emb.py
@@ -3,6 +3,21 @@
 import sys
 import pandas as pd
 from openai import OpenAI
+import re
+
+stop_words = [
+    'a', 'an', 'the', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'am', 'are', 'is',
+    'all','and','as','by','chore','feat','for','from','in','more','of','on','to','update','updated','we','when','with',
+    'fix','fixes','if','it','not','only','this','use',
+    'off','that','ci','github','action','actions','add','added','change','changes','create','yml',
+    'bot','new','so','some','try','workflow','workflows','also','now','signed','com','will','which','build'
+]
+
+def remove_stop_words(text, stop_words):
+    # 単語ごとに分割してストップワードを除去
+    words = re.findall(r'\b\w+\b', text.lower())
+    filtered_words = [word for word in words if word not in stop_words]
+    return ' '.join(filtered_words)
 
 args = sys.argv
 if len(args) != 4:
@@ -17,6 +32,8 @@ df = pd.read_csv(input_file_path)
 
 # log列を抽出
 logs = df["log"].values.astype('U')
+# stop wordsを除去
+logs = [remove_stop_words(log, stop_words) for log in logs]
 
 client = OpenAI(api_key=api_token)
 vectors = []
