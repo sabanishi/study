ff84e2f2ff2515e8eb23ee9c4096b5d2ff706e6a
Sabanishi <84651801+sabanishi@users.noreply.github.com>
2024-08-26 17:30:42 +0900

    Update delete_bot.py

--- GitHubActions/delete_bot.py
+++ GitHubActions/delete_bot.py
@@ -1,8 +1,5 @@
-#botによるコミットを削除する]
-
 import sys
 import pandas as pd
-
 
 args = sys.argv
 if len(args) != 4:
@@ -14,23 +11,35 @@ workflow_file_path = args[2]
 output_file_path = args[3]
 
 df = pd.read_csv(input_file_path)
-workdlow_df = pd.read_csv(workflow_file_path)
+workflow_df = pd.read_csv(workflow_file_path)
 
 output_df = pd.DataFrame()
+catch_df = pd.DataFrame()
+catch = 0
 len_df = len(df)
+
 # botによるコミットを削除
 for i, row in df.iterrows():
     is_bot = False
     commit_hash = row["commit_hash"]
-    #同じcommit_hashを持つ行をwordkflow_dfから取得
-    workflow = workdlow_df[workdlow_df["commit_hash"] == commit_hash]
+    # 同じcommit_hashを持つ行をworkflow_dfから取得
+    workflow = workflow_df[workflow_df["commit_hash"] == commit_hash]
     for _, workflow_row in workflow.iterrows():
-        #botによるコミットかどうかを判定
+        # botによるコミットかどうかを判定
         if "bot" in workflow_row["committer_name"]:
             is_bot = True
             break
     if not is_bot:
-        output_df = pd.concat([output_df, row])
-    print(f"{i+1}/{len_df}")
+        catch_df = pd.concat([catch_df, pd.DataFrame(row).T])
+        catch += 1
+        if catch >= 100:
+            catch = 0
+            output_df = pd.concat([output_df, catch_df])
+            catch_df = pd.DataFrame()
+        print(f"Add:{i+1}/{len_df}")
+    else:
+        print(f"Delete:{i+1}/{len_df}")  
 
+# 残りのデータを追加
+output_df = pd.concat([output_df, catch_df])
 output_df.to_csv(output_file_path, index=False)
