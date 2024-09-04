221ea38a01815988d110188567078605b05ab0ba
Sabanishi <84651801+sabanishi@users.noreply.github.com>
2024-09-04 11:00:50 +0900

    Update fetch_json_file.py

--- GitHubActions/Diff/fetch_json_file.py
+++ GitHubActions/Diff/fetch_json_file.py
@@ -33,16 +33,19 @@ output_folder_path = args[4]
 first_commit = args[5]
 
 #取ってくるコミットの数
-fetch_amount = 10000
+fetch_amount = 100000
 shuffle_seed = 9999
 
 print("読み込み開始")
 df = pd.read_csv(input_file_path)
 print("読み込み完了")
 
+# 重複を削除
+df = df.drop_duplicates(subset=['commit_hash'])
+
 #ファイルをシャッフル
 df = df.sample(frac=1,random_state=shuffle_seed)
-output_df = pd.DataFrame(columns=["commit_hash","url","log"])
+output_df = pd.read_csv(output_file_path)
 buffer = []
 
 i = 0
@@ -57,57 +60,55 @@ while(True):
     commit = df.iloc[i]
     i+=1
     commit_hash = commit["commit_hash"]
+
     if is_skip==False:
         if first_commit == "none" or commit_hash == first_commit:
             is_skip=True
         else:
             print(f"First Skip:({i}件目); {commit_hash}")
-            continue
+        continue
 
     url = "https://github.com/"+commit["repository"]+"/commit/"+commit_hash
     #コミットログを取得
-    while(True):
-        try:
-            #Committerがbotの場合はスキップ
-            if "bot" in commit["committer_name"]:
-                print(f"Skip(Bot Commit): {commit_hash}")
-                break
-            now_time = time.time()
-            (data,log) = get_commit_json(url,token)
-            #コミットログに「Merge」または「merge」または「#」が含まれている場合はスキップ
-            if "Merge" in log or "merge" in log or "#" in log:
-                print(f"Skip(Merge Commit): {commit_hash}")
-                break
+    try:
+        #Committerがbotの場合はスキップ
+        if "bot" in commit["committer_name"]:
+            print(f"Skip(Bot Commit): {commit_hash}")
+            continue
+        now_time = time.time()
+        (data,log) = get_commit_json(url,token)
+        #コミットログに「Merge」または「merge」または「#」が含まれている場合はスキップ
+        if "Merge" in log or "merge" in log or "#" in log:
+            print(f"Skip(Merge Commit): {commit_hash}")
+            continue
 
-            #jsonファイルを保存
-            json_path = output_folder_path +"/"+ commit_hash + ".json"
-            with open(json_path, mode='w') as f:
-                json.dump(data, f, indent=4)
-            
-            buffer.append({
-                "commit_hash": commit_hash,
-                "url": url,
-                "log": log
-            })
+        #jsonファイルを保存
+        json_path = output_folder_path +"/"+ commit_hash + ".json"
+        with open(json_path, mode='w') as f:
+            json.dump(data, f, indent=4)
+        
+        buffer.append({
+            "commit_hash": commit_hash,
+            "url": url,
+            "log": log
+        })
 
-            if len(buffer) >= batch_size:
-                output_df = pd.concat([output_df, pd.DataFrame(buffer)],ignore_index=True)
-                output_df.to_csv(output_file_path,index=False)
-                buffer.clear()
-            
-            success_count += 1
-            print(f"Success({success_count}件目): {commit_hash}")
+        if len(buffer) >= batch_size:
+            output_df = pd.concat([output_df, pd.DataFrame(buffer)],ignore_index=True)
+            output_df.to_csv(output_file_path,index=False)
+            buffer.clear()
+        
+        success_count += 1
+        print(f"Success({success_count}件目): {commit_hash}")
 
-            # 0.8秒待機
-            elapsed_time = time.time() - now_time
-            if elapsed_time < 0.8:
-                time.sleep(0.8-elapsed_time)
-                break
-        except Exception as e:
-            #エラーが発生した場合は1分待機
-                print(f"Error: {e}")
-                time.sleep(60)
-                break
+        # 0.8秒待機
+        elapsed_time = time.time() - now_time
+        if elapsed_time < 0.8:
+            time.sleep(0.8-elapsed_time)
+    except Exception as e:
+        #エラーが発生した場合は1分待機
+            print(f"Error: {e}")
+            time.sleep(60)
 
 if buffer:
     output_df = pd.concat([output_df, pd.DataFrame(buffer)],ignore_index=True)
