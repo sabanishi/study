262d31086ffe24d0cc952e073227f5b3663d1e67
sabanishi <84651801+sabanishi@users.noreply.github.com>
2024-11-05 13:02:54 +0900

    ファイルの内容をASTに変換するところまで実装

---
+++ LLMARP/src/test/java/SandboxTest.java
@@ -0,0 +1,48 @@
+import com.github.gumtreediff.client.Run;
+import com.github.gumtreediff.gen.jdt.JdtTreeGenerator;
+import org.junit.jupiter.api.Test;
+import com.github.gumtreediff.tree.Tree;
+
+import java.io.IOException;
+
+public class SandboxTest {
+
+    @Test
+    void sandboxTest(){
+        String oldSource="",newSource="";
+        try {
+            oldSource = TestUtils.read("Before.java");
+            newSource = TestUtils.read("After.java");
+        }catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("Before.java:");
+        System.out.println(oldSource);
+        System.out.println("\n\nAfter.java:");
+        System.out.println(newSource);
+
+        //2つのファイルをGumTreeを用いて抽象構文木に変換する
+        Run.initGenerators();
+        Tree beforeTree;
+        Tree afterTree;
+        try{
+            beforeTree = createTree(oldSource);
+            afterTree = createTree(newSource);
+        }catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        //結果を出力
+        System.out.println("\n\nBefore.java AST:");
+        System.out.println(beforeTree.toTreeString());
+        System.out.println("\n\nAfter.java AST:");
+        System.out.println(afterTree.toTreeString());
+    }
+
+    private Tree createTree(String source) throws Exception{
+        return new JdtTreeGenerator().generateFrom().string(source).getRoot();
+    }
+}
---
+++ LLMARP/src/test/java/TestUtils.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+public class TestUtils {
+
+    /**
+     *  引数のファイルをresourcesフォルダから読み込み、その内容を返す
+     * @param filePath
+     * @return
+     */
+    public static String read(String filePath) throws IOException {
+        ClassLoader classLoader = TestUtils.class.getClassLoader();
+        URL resource = classLoader.getResource(filePath);
+
+        return Files.lines(Paths.get(resource.getPath()), StandardCharsets.UTF_8)
+                .collect(Collectors.joining(System.getProperty("line.separator")));
+    }
+}
