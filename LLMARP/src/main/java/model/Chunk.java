9d21bbd1cae924b2ef4432331899742f567ae64d
sabanishi <84651801+sabanishi@users.noreply.github.com>
2024-11-07 14:33:59 +0900

    Chunk.java リファクタ

--- LLMARP/src/main/java/model/Chunk.java
+++ LLMARP/src/main/java/model/Chunk.java
@@ -1,84 +1,54 @@
 package model;
 
 import com.github.gumtreediff.tree.Tree;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import lombok.Value;
 import org.eclipse.jgit.diff.Edit;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
 @ToString(of = {"fileName","oldStatement","newStatement"})
 @RequiredArgsConstructor
+@Value
 public class Chunk {
-    @Getter
-    private final String fileName;
+    String fileName;
 
-    @Getter
-    private final Statement oldStatement;
-    @Getter
-    private final Tree oldAllTree;
-    @Getter(lazy = true)
-    private final Tree oldTree = extractSubTree(oldAllTree,oldStatement);
+    Statement oldStatement;
+    Statement newStatement;
 
-    @Getter
-    private final Statement newStatement;
-    @Getter
-    private final Tree newAllTree;
-    @Getter(lazy = true)
-    private final Tree newTree = extractSubTree(newAllTree,newStatement);
+    Pattern originalPattern;
+
+    List<Pattern> normalizedPatterns;
 
     public static Chunk of(final String fileName, final List<Statement> oldStatements, Tree oldAllTree,final List<Statement> newStatements, Tree newAllTree,Edit e){
         final List<Statement> oldSlice = oldStatements.subList(e.getBeginA(), e.getEndA());
         final List<Statement> newSlice = newStatements.subList(e.getBeginB(), e.getEndB());
 
-        int oldLineBegin,oldLineEnd,newLineBegin,newLineEnd;
-        int oldCharBegin,oldCharEnd,newCharBegin,newCharEnd;
+        int oldLineBegin = oldSlice.get(0).getLines().getBegin();
+        int oldLineEnd = oldSlice.get(oldSlice.size() - 1).getLines().getEnd();
+        int newLineBegin = newSlice.get(0).getLines().getBegin();
+        int newLineEnd = newSlice.get(newSlice.size() - 1).getLines().getEnd();
 
-        if(oldSlice.isEmpty()){
-            oldLineBegin = e.getBeginA() == oldStatements.size()
-                    ? oldStatements.get(oldStatements.size()-1).getLines().getEnd()
-                    : oldStatements.get(e.getBeginA()).getLines().getBegin();
-            oldLineEnd = oldLineBegin;
-
-            oldCharBegin = e.getBeginA() == oldStatements.size()
-                    ? oldStatements.get(oldStatements.size()-1).getChars().getEnd()
-                    : oldStatements.get(e.getBeginA()).getChars().getBegin();
-            oldCharEnd = oldCharBegin;
-        }else{
-            oldLineBegin = oldSlice.get(0).getLines().getBegin();
-            oldLineEnd = oldSlice.get(oldSlice.size()-1).getLines().getEnd();
-
-            oldCharBegin = oldSlice.get(0).getChars().getBegin();
-            oldCharEnd = oldSlice.get(oldSlice.size()-1).getChars().getEnd();
-        }
-
-        if(newSlice.isEmpty()) {
-            newLineBegin = e.getBeginB() == newStatements.size()
-                    ? newStatements.get(newStatements.size() - 1).getLines().getEnd()
-                    : newStatements.get(e.getBeginB()).getLines().getBegin();
-            newLineEnd = newLineBegin;
-
-            newCharBegin = e.getBeginB() == newStatements.size()
-                    ? newStatements.get(newStatements.size() - 1).getChars().getEnd()
-                    : newStatements.get(e.getBeginB()).getChars().getBegin();
-            newCharEnd = newCharBegin;
-        }else{
-            newLineBegin = newSlice.get(0).getLines().getBegin();
-            newLineEnd = newSlice.get(newSlice.size()-1).getLines().getEnd();
-
-            newCharBegin = newSlice.get(0).getChars().getBegin();
-            newCharEnd = newSlice.get(newSlice.size()-1).getChars().getEnd();
-        }
+        int oldCharBegin = oldSlice.get(0).getChars().getBegin();
+        int oldCharEnd = oldSlice.get(oldSlice.size() - 1).getChars().getEnd();
+        int newCharBegin = newSlice.get(0).getChars().getBegin();
+        int newCharEnd = newSlice.get(newSlice.size() - 1).getChars().getEnd();
 
         Statement oldStatement = Statement.joint(oldSlice, Range.of(oldLineBegin, oldLineEnd),Range.of(oldCharBegin,oldCharEnd));
         Statement newStatement = Statement.joint(newSlice, Range.of(newLineBegin, newLineEnd),Range.of(newCharBegin,newCharEnd));
 
-        return new Chunk(fileName, oldStatement,oldAllTree, newStatement,newAllTree);
+        Pattern originalPattern = Pattern.of(extractSubTree(oldAllTree,oldStatement),extractSubTree(newAllTree,newStatement));
+
+        List<Pattern> normalizedPatterns = new ArrayList<>();
+        normalizedPatterns.add(originalPattern);
+
+        return new Chunk(fileName, oldStatement, newStatement,originalPattern,normalizedPatterns);
     }
 
-    private Tree extractSubTree(Tree root,Statement statement){
+    private static Tree extractSubTree(Tree root,Statement statement){
         Range range = statement.getChars();
         final int begin = range.getBegin();
         final int end = range.getEnd();
